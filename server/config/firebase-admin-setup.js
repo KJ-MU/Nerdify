a80d44657a317e70dde04df985794e19a783c091
KJ-MU <gullssea5@gmail.com>
2024-06-26 19:43:48 +0300

    add cerdintiol of firebase to env file

--- server/config/firebase-admin-setup.js
+++ server/config/firebase-admin-setup.js
@@ -2,11 +2,11 @@ const { firestore } = require("firebase-admin");
 const { initializeApp, cert } = require("firebase-admin/app");
 const { getStorage } = require("firebase-admin/storage");
 
-const serviceAccountKey = require("../serviceAccountKey.json");
+const serviceAccount = require("../serviceAccountKey");
 
 // Initialize the Firebase Admin SDK
 exports.app = initializeApp({
-  credential: cert(serviceAccountKey),
+  credential: cert(serviceAccount),
   storageBucket: `gs://nerdify-3b431.appspot.com`,
 });
 
